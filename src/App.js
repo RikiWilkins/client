6a5566fbeabc2f52139fe04f75bf4263732c7f4e
e_jaros <e_jaros@utb.cz>
2023-02-02 22:33:51 +0100

    New way to access components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import {BeakerIcon, ZapIcon} from '@primer/octicons-react';
-import {Routes} from 'react-router-dom';
 import { useNavigate } from '@reach/router';
-import { Router, navigate } from '@reach/router';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { navigate } from '@reach/router';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom";
 //import FavAlbum from './Components/FavAlbums.js';
 import FavAlbums from './Components/FavAlbums.js';
-import { Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 const CLIENT_ID = "f1d4793b803e47408bb102185056dcc3";
 const CLIENT_SECRET = "74d09c05be44444388a27e92691eccc4";
@@ -18,6 +23,17 @@ const CLIENT_SECRET = "74d09c05be44444388a27e92691eccc4";
 const navigateToFav = () => { 
   navigate('/favalbums'); 
 };
+
+ReactDOM.render(/*(
+  
+  <BrowserRouter>
+    <div>
+      <Route path="/favalbums" element={<FavAlbums />} />
+      <Route path="/" element={<App />} />
+    </div>
+  </BrowserRouter>
+  
+), */document.getElementById('app'))
 
 export default function App({ navigation }) {
   const [ searchInput, setSearchInput ] = useState('');
@@ -138,7 +154,8 @@ export default function App({ navigation }) {
       <Container className="d-flex justify-content-end align-items-end position-fixed mb-3" style={{ bottom: "15px", right: "15px" }}>
         <button type="button" onClick={navigateToFav} className="btn btn-circle btn-primary mb-4 p-3">
           <Router>
-            <FavAlbums path="/favalbums" />
+            <Route exact path="/" element={<App/>} />
+            <Route exact path="/favalbums" element={<FavAlbums/>} />
           </Router>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
             <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
