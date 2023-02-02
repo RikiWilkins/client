83c510f274e767a1f6dedfb5ac202cfbefbc0116
e_jaros <e_jaros@utb.cz>
2023-02-02 19:18:16 +0100

    New button for transition to favorites pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
 import { useState, useEffect } from 'react';
+import {BeakerIcon, ZapIcon} from '@primer/octicons-react'
 
 const CLIENT_ID = "f1d4793b803e47408bb102185056dcc3";
 const CLIENT_SECRET = "74d09c05be44444388a27e92691eccc4";
@@ -92,12 +93,12 @@ function App() {
       </Container>
 
       <Container>
-        <Row className="mx-2 row row-cols-4">
+        <Row className="mx-2 row row-cols-4 d-flex justify-content-center">
           {albums.map((album, i) => {
             console.log(album);
             return (
-              <Card>
-                <Card.Img src={album.images[0].url} />
+              <Card className='m-1'>
+                <Card.Img className='mt-3' src={album.images[0].url} />
                 <Card.Body>
                   <Card.Title>{album.name}</Card.Title>
                   <div style={{
@@ -121,7 +122,14 @@ function App() {
           })}
           
         </Row>
-        
+      </Container>
+
+      <Container className="d-flex justify-content-end align-items-end position-fixed mb-3" style={{ bottom: "15px", right: "15px" }}>
+        <button type="button" className="btn btn-circle btn-primary mb-4 p-3">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path>
+          </svg>
+        </button>
       </Container>
     </div>
   );
